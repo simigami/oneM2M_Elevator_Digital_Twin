5b115ac93f879a5f4ca5fd1fc1819c0c1406128d
simigami <46891822+simigami@users.noreply.github.com>
2024-07-02 01:55:06 +0900

    Model Updated

--- Image Detection/Roboflow/SAI Elevator.v1i.yolov8/README.dataset.txt
+++
@@ -1,6 +0,0 @@
-# SAI Elevator > 2023-07-09 10:36pm
-https://universe.roboflow.com/elevator-rigpk/sai-elevator
-
-Provided by a Roboflow user
-License: CC BY 4.0
-
---
+++ Resources/Image Detection (Client)/test.py
@@ -0,0 +1,30 @@
+import os
+import cv2
+from ultralytics import YOLO
+os.environ["WANDB_MODE"] = "dryrun"
+
+def test():
+    new_model = YOLO(r'best.pt')
+    test_folder_dir = r'E:\ML\Elevator Git\Effective-Elevator-Energy-Calculation-for-SejongAI-Center\Resources\Image Detection (Client)\Test Images'
+
+    test_images = os.listdir(test_folder_dir)
+    for test_image in test_images:
+        test_image_dir = os.path.join(test_folder_dir, test_image)
+        result = new_model(test_image_dir)
+
+        plots = result[0].plot()
+
+        # Change Images Size
+        plots = cv2.resize(plots, (620, 620))
+        # Save Image
+        cv2.imwrite(f'{test_image_dir}_result.jpg', plots)
+
+if __name__ == '__main__':
+    # model = YOLO('yolov8x.pt')
+    # model.train(
+    #     data='./train2.yaml',
+    #     batch=16,
+    #     epochs=100
+    # )
+    test()
+
